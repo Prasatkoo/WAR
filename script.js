a1b527548d6a5609698aba30a4243f72d2918da7
Prasatkoo <filip.hasek@educanet.cz>
2022-09-30 09:05:01 +0200

    Ukol

---
+++ script.js
@@ -0,0 +1,17 @@
+function minToSec(a) {
+    console.log(a * 60)
+}
+
+minToSec(32)
+function aZaroven(bol, bol1) {
+return (!!bol && !!bol1) === true;
+}
+console.log(aZaroven("Str",1))
+
+function newFunction(x, y) {
+    if (x <= 0 || y <= 0){
+        console.log("-1")
+    }
+    else console.log(x*y)
+}
+newFunction(8,4)
